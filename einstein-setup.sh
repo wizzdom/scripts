898dfaea3c2d9453b08a4af10766d7b2b9ccfeeb
wizzdom <dom@wizzdom.xyz>
2022-10-08 23:41:20 +0100

    fix typo

--- einstein-setup.sh
+++ einstein-setup.sh
@@ -1,17 +1,18 @@
 #!/user/bin/env sh
 
-echo -n "Welcome!"
-echo -n "Enter your DCU username:"
+echo "Welcome!"
+echo "Enter your DCU username:"
 read username
 
-echo -n "Installing ZSH..."
+echo "Installing ZSH..."
 sudo apt update && sudo apt-get install zsh
 
-echo -n "Downloading and installing Einstein"
+echo "Downloading and installing Einstein"
 wget "https://einstein.computing.dcu.ie/res/einstein" && sudo install -v -m 0755 einstein /usr/local/bin
 
-echo -n "Setting your Einstein username..."
+echo "Setting your Einstein username..."
 echo "EINSTEIN_USERNAME='"$username"'" >> $HOME/.bashrc
 
-echo -n "Done."
-echo -n "Restart your shell (terminal) to use Einstein"
+echo "Done."
+echo "Restart your shell (terminal) to use Einstein"
+exit
