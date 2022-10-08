2ea7d1696d177d3a7661c34d12427051e5e85171
wizzdom <dom@wizzdom.xyz>
2022-10-08 23:52:33 +0100

    refactor script

--- einstein-setup.sh
+++ einstein-setup.sh
@@ -1,18 +1,27 @@
 #!/user/bin/env sh
 
+echo
 echo "Welcome!"
-echo "Enter your DCU username:"
-read username
+echo 
+read -p "Enter your DCU username:" username
+echo "Your DCU username:"$username
 
+echo
 echo "Installing ZSH..."
 sudo apt update && sudo apt-get install zsh
 
-echo "Downloading and installing Einstein"
-wget "https://einstein.computing.dcu.ie/res/einstein" && sudo install -v -m 0755 einstein /usr/local/bin
+echo "Downloading Einstein..."
+wget "https://einstein.computing.dcu.ie/res/einstein"
 
+echo
+echo "Installing Einstein (may need your Linux password)..."
+sudo install -v -m 0755 einstein /usr/local/bin
+
+echo
 echo "Setting your Einstein username..."
 echo "EINSTEIN_USERNAME='"$username"'" >> $HOME/.bashrc
 
 echo "Done."
 echo "Restart your shell (terminal) to use Einstein"
+sleep 20
 exit
