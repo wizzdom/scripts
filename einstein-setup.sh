f74b4f6c005e22214329a964209e8c633ffd29e5
wizzdom <dom@wizzdom.xyz>
2022-10-13 23:45:57 +0100

    add more info and checks, suopport zsh and bash, and support OSes without apt (including macOS, theoretically)

--- einstein-setup.sh
+++ einstein-setup.sh
@@ -1,19 +1,36 @@
 #!/user/bin/env bash
 
+clear
 echo
 echo "Welcome!"
 echo 
-sleep 5
+sleep 2
+echo "Please enter your DCU username when prompted..."
+sleep 1
+echo
 read -p "Enter your DCU username:" dcu_username
-
 
 sleep 2
 echo "Your DCU username:" $dcu_username
 
 echo
-echo "Installing ZSH..."
-sudo apt update && sudo apt-get install zsh
+echo "Checking some things first..."
+echo
+if command -v apt &> /dev/null
+then
+    echo "Installing ZSH..."
+    echo "This will ask for your Linux password"
+    sudo apt update
+    sudo apt install zsh
+else
+    echo "APT package manager could not be found"
+    echo "you may need to install zsh seperately if einstein fails to run"
+    echo "Continuing anyway..."
+    echo "(This is normal for MacOS)"
+    sleep 5
+fi
 
+echo
 echo "Downloading Einstein..."
 wget "https://einstein.computing.dcu.ie/res/einstein"
 
@@ -23,10 +40,12 @@ sudo install -v -m 0755 einstein /usr/local/bin
 
 echo
 echo "Setting your Einstein username..."
-echo "EINSTEIN_USERNAME='"$dcu_username"'" >> $HOME/.bashrc
+[[ -f "$HOME/.bashrc" ]] && echo "export EINSTEIN_USERNAME='$dcu_username'" >> $HOME/.bashrc
+[[ -f "$HOME/.zshrc" ]] && echo "export EINSTEIN_USERNAME='$dcu_username'" >> $HOME/.zshrc
 
 echo "Done."
 echo "Restart your shell (terminal) to use Einstein"
-source $HOME/.bashrc
-sleep 5
+[[ -f "$HOME/.bashrc" ]] && source $HOME/.bashrc
+[[ -f "$HOME/.zshrc" ]] && source $HOME/.zshrc
+sleep 2
 exit
