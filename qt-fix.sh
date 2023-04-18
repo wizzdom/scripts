d90a438622df541583b7e5344104c18129918acb
wizzdom <dom@wizzdom.xyz>
2023-04-18 14:46:30 +0100

    add: qt-fix.sh

---
+++ qt-fix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A script to fix when custom QT styles break due to a QT update
+# This script recompiles them against the new version of QT
+
+aurhelper=paru
+echo "rebuilding qt5 plugins"
+$aurhelper -aS --rebuild qt5-styleplugins qt5gtk2
+
+echo "rebuilding qt6 plugins"
+$aurhelper -aS --rebuild qt6gtk2
