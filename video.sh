65e6adaa73f1fcba413bfcfd71d7fdfcb146ae31
wizzdom <dom@wizzdom.xyz>
2024-01-01 20:22:48 +0000

    video wallpaper: restructuring and make more reusable

--- video.sh
+++ video.sh
@@ -1,1 +1,30 @@
-mpvpaper -o "no-audio loop speed=0.8" eDP-1 ~/Videos/nobara.mp4
+#!/usr/bin/env bash
+# Script to set a video as wallpaper using mpvpaper
+# Dependencies: mpv, mpvpaper and a wayland compositor
+# Author: wizzdom
+
+# kill swaybg if it is running
+if ps -C swaybg
+then
+    pkill -9 swaybg
+fi
+
+# kill mpvpaper if it is running
+if ps -C mpvpaper
+then
+    pkill -9 mpvpaper
+fi
+
+# set monitor
+MONITOR="HDMI-A-1"
+
+# checks if the first argument is empty, if it is, then it sets the video to the default
+if [ -z $1 ]
+then
+    VIDEO=$HOME/Videos/nobara.mp4
+else
+    VIDEO="$1"
+    fi
+
+# set video wallpaper
+mpvpaper -o "no-audio loop speed=0.8" $MONITOR "$VIDEO"
